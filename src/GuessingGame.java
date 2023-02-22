692f15407fb992a2d182fc4b659b34fe690d6ad1
Amee <ameecova@yahoo.com>
2023-02-22 21:11:23 +0100

    Get user name and greet

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class GuessingGame {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
         System.out.println("Welcome to the Guessing Game!");
+        System.out.println("\nWhat is your name?");
+        String userName = myObj.nextLine();
+
+        System.out.println("\nHello, " + userName + "!");
     }
 }
