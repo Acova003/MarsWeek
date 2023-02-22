4ec1ceaa46f81c586615a3e44c0e160a8232130a
Amee <ameecova@yahoo.com>
2023-02-22 20:54:31 +0100

    Finish landingCheck conditional logic

--- src/Mars.java
+++ src/Mars.java
@@ -19,13 +19,21 @@ public class Mars {
             System.out.println("ERROR!!! Flight plan already set. Landing on the Plain");
         }
     }
-    public static boolean landingCheck(int minutesLeft) {
+    public static boolean landingCheck(int minutesLeft) throws InterruptedException {
         for (int minute = 0; minute <= minutesLeft; minute++) {
             if (minute % 2 == 0 && minute % 3 == 0) {
                 System.out.println("Keep Center");
-            } else {
-                
+            } else if (minute % 2 == 0){
+                System.out.println("Right");
+            } else if (minute % 3 == 0){
+                System.out.println("Left");
+            } else{
+                System.out.println("Calculating");
             }
+            Thread.sleep(250);
+
         }
+        System.out.println("Landed");
+        return false;
     }
 }
