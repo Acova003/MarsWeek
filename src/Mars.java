a9df125e9bf1edffbcce84b61220aa33b39cbfe7
Amee <ameecova@yahoo.com>
2023-02-22 20:13:15 +0100

    Set variables, create calc, check landing location

--- src/Mars.java
+++ src/Mars.java
@@ -1,5 +1,22 @@
 public class Mars {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String colonyName = "Sitges";
+        int shipPopulation = 300;
+        double meals = 4000.00;
+        boolean landing = true;
+
+        meals -= (shipPopulation * 0.75);
+        meals -= (shipPopulation * 0.75);
+        System.out.println(meals);
+        meals += (meals * .5);
+        shipPopulation += 5;
+
+        String landingLocation = "The Plain";
+
+        if (landingLocation.equalsIgnoreCase("The Plain")) {
+            System.out.println("Bbzzz Landing on the Plain");
+        } else {
+            System.out.println("ERROR!!! Flight plan already set. Landing on the Plain");
+        }
     }
 }
