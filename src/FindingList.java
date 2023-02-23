73f4a2d034a92d7dc9fcdf827f99a015763364d2
Amee <ameecova@yahoo.com>
2023-02-23 01:35:03 +0100

    Store all rock samples

--- src/FindingList.java
+++ src/FindingList.java
@@ -1,5 +1,25 @@
 public class FindingList {
     public FindingList() throws InterruptedException {
         Thread.sleep(500);
+
+        System.out.println("Welcome back to Earth!");
+
+        ArrayList<String> rockList = new ArrayList<String>();
+
+        System.out.println("All the rock data has been downloaded.");
+
+        rockList.add("rock");
+        rockList.add("weird rock");
+        rockList.add("smooth rock");
+        rockList.add("not rock");
+
+        System.out.println(rockList);
+
+        System.out.println("Wait a second that last on is not a rock we need to delete that one from the list");
+
+        rockList.remove("not rock");
+
+        System.out.println(rockList);
+        System.out.println("Perfect");
     }
 }
